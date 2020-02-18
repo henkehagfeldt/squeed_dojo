817a6a4a3455e59840ec8b73114bbef104b0735f
Henrik Hagfeldt <hagfeldt@student.chalmers.se>
2020-02-18 09:03:17 +0100

    squeed init

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Test x = new Test(2);
+        x.inc();
+        x.inc();
+        System.out.println(x.getx());
+    }
+}
---
+++ src/com/company/Test.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Test {
+    private int x;
+
+    public Test(int x){
+        this.x = x;
+    }
+
+    public int getx(){
+        return(this.x);
+    }
+
+    public void inc(){
+        this.x++;
+    }
+}
